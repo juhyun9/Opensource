22cf3f25bfbc00f592ba293903bbfb9e22769f66
juhyun9 <990001@stu.jejunu.ac.kr>
2024-06-24 13:14:07 +0900

    배포 위한 소스 이동

---
+++ Opensource_Project/static/share.js
@@ -0,0 +1,33 @@
+const url = 'https://github.io/Opensource_Project';
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '올레코스 유형 테스트 결과';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.png';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  share.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ]
+  });
+}
